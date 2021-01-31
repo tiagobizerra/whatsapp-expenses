debd66ba7ce926ff4a8641fac6223f0793a5560f
Tiago Lima <tiago@tiagolima.com.br>
2021-01-30 22:52:41 -0300

    new functions - basic checks to validate expenses

---
+++ whatsapp_expenses/expenses.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python
+
+'''
+Expenses functions
+'''
+
+def format_expense(raw_expense):
+    return None
+
+def validate_expense(formated_expense):
+
+    '''
+    Validate a expense
+    '''
+
+    if len(formated_expense) == 2 and (formated_expense[0] and formated_expense[1]):
+        return True
+
+    return False
+
+def create_expense(expense):
+
+    '''
+    Create a new expense from string passed
+    '''
+
+    for data in expense:
+        new_expense = {
+                'cost': data[0],
+                'name': data[1:]
+            }
+
+    return new_expense
--- whatsapp_expenses/wpe.py
+++ whatsapp_expenses/wpe.py
@@ -7,6 +7,7 @@ WhatsApp Expenses Modules
 import logging
 import platform
 import sys
+from expenses import create_expense, validate_expense
 
 class HostnameFilter(logging.Filter):
 
@@ -33,6 +34,23 @@ LOGGER = logging.getLogger()
 LOGGER.addHandler(HANDLER)
 LOGGER.setLevel(logging.INFO)
 
+def create_report_metadata(metadata):
+
+    '''
+    Generate metadata for a new expense from string passed
+    Metadata returned as a dict includes: date, time, user
+    '''
+
+    raw_metadata = metadata.split(' ')[0:3]
+
+    report_metadata = {
+            "date": raw_metadata[0][1:-1],
+            "time": raw_metadata[1].replace(']',''),
+            "user": raw_metadata[2].replace(':','')
+        }
+
+    return report_metadata
+
 
 def wpefile(filename):
 
@@ -40,10 +58,56 @@ def wpefile(filename):
     Main Function
     '''
 
+    valid_expenses = 0
+    invalid_expenses = 0
+
     logging.info('Reading file %s', filename)
     try:
         wpe_filename = open(filename)
     except IOError as err:
         logging.error('File not found: %s\n%s', filename, err)
 
-    print(wpe_filename)
+    lines = wpe_filename.readlines()
+    count = 0
+
+    for line in lines:
+
+        count += 1
+
+        if line.startswith('['):
+
+            logging.info('New expense! Creating report metadata')
+            report_metadata = create_report_metadata(line)
+            logging.info(report_metadata)
+
+            expense = line.split(' ')[3:]
+            expense[-1] = expense[-1].rstrip("\n")
+
+            logging.info('Checking if expense is valid: %s', expense)
+
+            if bool(validate_expense(expense)) is True:
+
+                logging.info('Valid! Creating expense: %s', expense)
+                valid_expenses += 1
+                create_expense(expense)
+
+            else:
+                logging.info('Invalid! Skipping expense: %s', expense)
+                invalid_expenses += 1
+
+        else:
+
+            logging.info('Previous')
+            expense = line.split(' ')
+
+            if bool(validate_expense(expense)) is True:
+
+                logging.info('Valid! Creating expense: %s', expense)
+                valid_expenses += 1
+                create_expense(expense)
+
+            else:
+                logging.info('Invalid! Skipping expense: %s', expense)
+                invalid_expenses += 1
+
+    wpe_filename.close()
